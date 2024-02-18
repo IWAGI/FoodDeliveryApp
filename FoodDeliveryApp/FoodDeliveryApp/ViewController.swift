804bf0cb4fa5fb3d88ce8ec204b9539c149cbea8
Mikhail <m.kushakov@movavi.com>
2024-02-18 13:55:48 +0700

    Creared a project

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by m.kushakov on 18.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
